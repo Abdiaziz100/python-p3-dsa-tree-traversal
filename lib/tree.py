d20d873d0ef7614a0b981fcbf9b573b9a0795f9f
Abdiaziz100 <abdiazizmahat100@gmail.com>
2025-09-03 14:10:04 +0300

    kiki

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,15 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        if self.root is None:
+            return None
+        stack = [self.root]
+        while stack:
+            node = stack.pop()
+            if node.get('id') == id:
+                return node
+            for child in reversed(node.get('children', [])):
+                stack.append(child)
+        return None
